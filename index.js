9c84013da7e7d921afbda80f1644f1bc3ae6c4c6
Krystian D <dol_kryku@interia.pl>
2020-03-23 12:15:23 +0100

    index fixes

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const testAddon = require('./build/Release/canlistener.node');
+const isotp = require('./build/Release/canlistener.node');
 
-var isotp = new testAddon.IsotpWrapper("can0")
-
-isotp.read(function (data) {
-  console.log(data);
-}, 0x321, 0x123);
-
-
-//setInterval(() => isotp.send("lol2", 0x321, 0x123), 1000)
+exports.module = isotp
